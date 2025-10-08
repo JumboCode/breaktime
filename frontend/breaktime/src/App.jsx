03a4cc9d2115e57211d4063a8e1acb11e8d8c31c
ermiayn3962 <113037878+ermiayn3962@users.noreply.github.com>
2025-10-08 19:05:20 -0400

    Added Example Component

--- frontend/breaktime/src/App.jsx
+++ frontend/breaktime/src/App.jsx
@@ -4,21 +4,15 @@
  * 
  */
 
-import { useState } from 'react';
 import PWABadge from './PWABadge.jsx';
+import ExampleComponent from './components/exampleComponent.jsx';
 
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Breaktime</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          You have clicked me {count}
-        </button>
-      </div>
+      <ExampleComponent/>
 
       <PWABadge />
     </>
