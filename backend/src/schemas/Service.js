2777a20112164a1bb69fd6a44667068ea2a1a40a
sean <124487151+seanozalpasan@users.noreply.github.com>
2025-10-15 22:15:37 -0400

    B1C Create Schema for Service (#4)
    * just some test comments
    
    * added the Service.js file from the jira ticket
    
    added everything required + kept the id string. keeping id in the schema is useful
    
    * cleaned up exampleSchema

---
+++ backend/src/schemas/Service.js
@@ -0,0 +1,11 @@
+const Joi = require('joi');
+
+const servicesSchema = Joi.object({
+   Rules: Joi.string(),
+   Description: Joi.string(),
+   Title: Joi.string(),
+   Availability : Joi.string(),
+   id: Joi.string()
+});
+
+module.exports = { servicesSchema };
