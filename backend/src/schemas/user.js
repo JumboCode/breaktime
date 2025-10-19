96c8d2ad3f3dd9e542a7e0f8b9aa94749c768e97
ermiayn3962 <113037878+ermiayn3962@users.noreply.github.com>
2025-10-19 18:37:06 -0400

    Added Clerk Config

--- backend/src/schemas/user.js
+++ backend/src/schemas/user.js
@@ -3,9 +3,10 @@ const Joi = require('joi');
 const userSchema = Joi.object({
     firstName: Joi.string().required(),
     lastName: Joi.string().required(),
+    password: Joi.number.integer().positive().required(),
     age: Joi.number().integer().positive().required(),
     gender: Joi.string().required(),
-    ethnicity: Joi.string().required(),
+    race: Joi.string().required(),
     zone: Joi.string().required()
 });
 
