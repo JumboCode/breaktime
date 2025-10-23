c85706404f09b95ae3f6fa4cd7b451aad753ffa2
Emily Yuan <emyuans@yahoo.com>
2025-10-22 21:40:17 -0400

    add new user to mongo and clerk

--- backend/src/schemas/user.js
+++ backend/src/schemas/user.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const userSchema = Joi.object({
     firstName: Joi.string().required(),
     lastName: Joi.string().required(),
-    password: Joi.number.integer().positive().required(),
+    password: Joi.string().required(),
     age: Joi.number().integer().positive().required(),
     gender: Joi.string().required(),
     race: Joi.string().required(),
