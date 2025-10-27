ee87fdb46ca2a64d2b09bcec9893cd05da7eb42d
Emily Yuan <emyuans@yahoo.com>
2025-10-26 21:33:15 -0400

    adding staff router

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -8,34 +8,41 @@ const mongodbPromise = require('../utils/mongodb');
 const { userSchema } = require('../schemas/user');
 const { clerkClient } = require('@clerk/clerk-sdk-node'); 
 
-
+// Post Method Endpoint - Add user to Mongo and Clerk
 router.post('/', async (req, res) => {
     try {
+        // Get correct database collection in mongoDB, store to variables
         const client = await mongodbPromise;
         const database = client.db('requests');
         const collection = database.collection('accounts');
     
+        // Validate body of request
         const { error } = userSchema.validate(req.body);
         
         if (error) {
                 return res.status(400).send(error.details[0].message);
         } else {
+                // Set req body to individual fields
                 const { firstName, lastName, password, age, gender, race, zone } = req.body;
 
+                // Generate user ID using 8 digit random num
                 let username = username_generation();
                 while (await collection.findOne({ username })) {
                         username = username_generation();
                 }
-            
+                
                 const newUser = {
                         firstName, 
                         lastName,
                         username,
-                        permissionLevel: 0
+                        permissionLevel: 0 // Populate as undef
+                        // Validated and changed later
                 };
 
+                // Await insertion of the new user into mongoDB
                 const document = await collection.insertOne(newUser);
 
+                // For CLERK = populate mostly the same fields
                 const clerkUser = {
                         username: username,
                         password: password,
@@ -48,6 +55,7 @@ router.post('/', async (req, res) => {
                         },
                 };
 
+                // Await clerk return, return success
                 const user = await clerkClient.users.createUser(clerkUser);
                 return res.status(200).json({ message: 'User created successfully',  _id: document.insertedId, user });
 
@@ -62,12 +70,14 @@ router.post('/', async (req, res) => {
     }
 });
 
+// Generate a username, append string so it works with Clerk
+// Example: YA_12345678
 function username_generation() {
         rand = Math.floor(Math.random() * 100000000);
         return 'YA_' + String(rand).padStart(8, '0');
 }
 
-
+// Export this module so it's available to other users
 module.exports = router;
 
 
