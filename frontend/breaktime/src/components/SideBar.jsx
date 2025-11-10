bf5cc0f9a7e9dff26b6fb10968e6f3b9ec94d328
hbritt01 <henry.brittan@tufts.edu>
2025-11-09 21:07:32 -0500

    Added toggle for sideBar

--- frontend/breaktime/src/components/SideBar.jsx
+++ frontend/breaktime/src/components/SideBar.jsx
@@ -8,18 +8,8 @@ function SideBar() {
         numNotifications: 0 // Get notification data from notifications tab??
     });
 
-    // const [error, setError] = useState("");
-
-    // const handleChange = (event) => {
-    //     const { name, value } = event.target;
-    //     setNotificationData((prevData) => ({
-    //         ...prevData,
-    //         // if (value === 'notification') {
-    //         //     [name]: value + 1,
-    //         // }
-                
-    //     }));
-    // };
+    const isSidebarOpen = true;
+    
     return (
         <div className="flex flex-col h-screen bg-indigo-purple p-3 w-md">
             <h1 className="text-white text-2xl font-semibold mb-6">Welcome Back Labubu!</h1>
