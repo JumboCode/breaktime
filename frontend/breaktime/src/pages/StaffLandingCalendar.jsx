4b5752dbbe8688e7c914ca361583dc71d245f840
hbritt01 <henry.brittan@tufts.edu>
2025-11-10 19:58:46 -0500

    Changed back contact info

--- frontend/breaktime/src/pages/StaffLandingCalendar.jsx
+++ frontend/breaktime/src/pages/StaffLandingCalendar.jsx
@@ -5,13 +5,6 @@ export default function StaffLandingCalendar() {
     return (
         <div className="bg-indigo-purple h-screen w-screen">
             <NavBar />
-            <div className="text-light-purple-subtle absolute bottom-0 p-[30px]">
-                    Contact Us:
-                    <br />
-                    (508) 319 - 1679
-                    <br />
-                    info@breaktime.org
-                </div>
         </div>
     );
 }
