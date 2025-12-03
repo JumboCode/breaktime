1035f5c777b095fa0862cae449aab9a7d2bc07e3
Emily Yuan <emyuans@yahoo.com>
2025-12-02 19:57:47 -0500

    finished delete endpoint

--- backend/src/schemas/notification.js
+++ backend/src/schemas/notification.js
@@ -4,11 +4,11 @@ const notificationSchema = Joi.object({
    userID: Joi.string().required(),
    notificationID: Joi.number(),
    type : Joi.string()
-         .valid('BOOKING_CREATED', 'BOOKING_CONFIRMED', 'BOOKING_CANCELLED', 'BOOKING_REMINDER', 'ALERT')
-         .required(),
-   title: Joi.string().required(),
-   message: Joi.string().required(),
-   timestamp: Joi.date().required()
+         .valid('BOOKING_CREATED', 'BOOKING_CONFIRMED', 'BOOKING_CANCELLED', 'BOOKING_REMINDER', 'ALERT'),
+         
+   title: Joi.string(),
+   message: Joi.string(),
+   timestamp: Joi.date()
 });
 
 module.exports = { notificationSchema };
