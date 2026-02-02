6117b7a265b9e37b4e14c2f1354686412243011f
Emily Yuan <emyuans@yahoo.com>
2026-02-02 13:43:36 -0500

    ya landing page components

---
+++ frontend/breaktime/src/components/popup/LandingPagePopup.jsx
@@ -0,0 +1,84 @@
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { ChevronUp, ChevronDown } from 'lucide-react';
+import { useState } from "react";
+import ServiceImage from "../../assets/popup-icons/ServiceImage.png";
+import ButtonGoBack from "../../assets/popup-icons/ButtonGoBack.png";
+import BookButton from "../../assets/popup-icons/BookButton.png";
+
+
+function LandingPagePopup() {
+    const [expandedSection, setExpandedSection] = useState("expectations");
+    const sections = [
+    {
+        id: "expectations",
+        title: "Expectations & Rules",
+        content: [
+        "1. Make sure you clean before you use",
+        "2. Make sure you clean again before you use",
+        "3. Please make sure you clean before you use",
+        ],
+    },
+        { id: "provided", title: "What's Provided", content: ["TBD"] },
+        { id: "bring", title: "What You Need to Bring", content: ["TBD"] },
+        { id: "notices", title: "Notices / Messages", content: ["TBD"] },
+    ];
+
+    
+    const toggleSection = (id) => {
+        setExpandedSection((prev) => (prev === id ? null : id));
+    };
+
+    return (
+        <div className="absolute bottom-2 top-16 left-20 w-[1250px] h-[680px] bg-white
+        border-1 border-solid border-[#B27DED] rounded-lg font-poppins cursor-pointer">
+            <img
+                src={ButtonGoBack}
+                alt="Go Back Button"
+                className="w-[150px] h-[47px] mb-4 mt-2 ml-2"
+            />
+
+            <div className="flex flex-col">
+                <h2 className = "text-6xl font-thin text-[#B27DED] text-align ml-30 font-poppins mt-10">
+                Shower <br></br>Service</h2>
+                <img
+                    src={ServiceImage}
+                    alt="Service"
+                    className="w-[300px] ml-30 mt-7"
+                />
+                <img
+                    src={BookButton}
+                    alt="Book button"
+                    className="w-[150px] h-[47px] ml-30 mt-7"
+                />
+            </div>
+
+            <div className="absolute top-60 right-10">
+                <div className="w-[520px] mr-30 mt-7">
+                    {sections.map((section) => {
+                        const isOpen = expandedSection === section.id;
+                    return (
+                        <div key={section.id} className="mb-8">
+                        {/* header row */}
+                        <button
+                            type="button"
+                            onClick={() => toggleSection(section.id)}
+                            className="
+                            w-full flex items-center justify-between
+                            text-left
+                            text-[22px] font-medium text-[#2F2F2F]
+                            "
+                        >
+                            <span>{section.title}</span>
+                            {isOpen ? <ChevronDown /> : <ChevronRight />}
+                        </button>
+                        </div>
+                    );
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+}
+export default LandingPagePopup;
