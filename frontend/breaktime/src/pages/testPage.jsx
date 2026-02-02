4b1d0a58704bdedb8842984c95b1698bb97e5717
Emily Yuan <emyuans@yahoo.com>
2026-02-02 13:43:36 -0500

    ya landing page components

---
+++ frontend/breaktime/src/pages/testPage.jsx
@@ -0,0 +1,10 @@
+import LandingPagePopup from "../components/popup/LandingPagePopup";
+import React from "react";
+
+export default function TestPage() {
+  return (
+    <div>
+      <LandingPagePopup />
+    </div>
+  );
+}
