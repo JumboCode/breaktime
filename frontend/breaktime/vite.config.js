28f166ac1f46a86bed421740316ed915513fc489
alexanderviolet <alexander.violet@tufts.edu>
2026-02-01 21:15:20 -0500

    messed around with vite and api call suggestions (in progress)

--- frontend/breaktime/vite.config.js
+++ frontend/breaktime/vite.config.js
@@ -5,46 +5,55 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
 
-    // https://vite-pwa-org.netlify.app/guide/pwa-minimal-requirements#web-app-manifest
-    manifest: { 
-      name: 'Breaktime',
-      short_name: 'Breaktime',
-      description: 'Breaktime',
-      theme_color: '#ffffff',
-      icons: [
-        {
-          src: '/favicon.svg',
-          size: '192x192',
-          type: 'image/png' 
-        },
-        {
-          src: '/favicon.svg',
-          size: '512x512',
-          type: 'image/png' 
-        }
-      ],
-    },
+    // https://vite-pwa-org.netlify.app/guide/pwa-minimal-requirements#web-app-manifest
+    manifest: { 
+      name: 'Breaktime',
+      short_name: 'Breaktime',
+      description: 'Breaktime',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/favicon.svg',
+          size: '192x192',
+          type: 'image/png' 
+        },
+        {
+          src: '/favicon.svg',
+          size: '512x512',
+          type: 'image/png' 
+        }
+      ],
+    },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
 
-    devOptions: {
-      enabled: true,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
 })
