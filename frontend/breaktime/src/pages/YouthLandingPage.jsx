74fa8393cc04558c82380680d52df097351b5ed2
rockyboo3 <caitlyn.parrishlewis@gmail.com>
2026-02-04 21:09:32 -0500

    Remove floating semi-colon from Youth Landing page

--- frontend/breaktime/src/pages/YouthLandingPage.jsx
+++ frontend/breaktime/src/pages/YouthLandingPage.jsx
@@ -83,7 +83,7 @@ export default function HomePage() {
                                     </div>
                                 ))
                             )
-                        )};
+                        )}
                     </div>
                 </div>
             </div>
