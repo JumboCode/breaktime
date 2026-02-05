502686808917d7dc6c85428fa01459c407046359
rockyboo3 <caitlyn.parrishlewis@gmail.com>
2026-02-05 16:23:01 -0500

    F5A: Carousel (not infinite) + begin hover for services

--- frontend/breaktime/src/components/YACarouselItem.jsx
+++ frontend/breaktime/src/components/YACarouselItem.jsx
@@ -1,33 +1,17 @@
 import { useState } from "react";
 
 const CarouselItem = ({ name, image }) => {
-    // class NextBooking {
-    //     constructor(id, title, message) {
-    //         this.id = id;
-    //         this.title = title;
-    //         this.message = message;
-    //     }
-    // }
+    const [isHovering, setIsHovering] = useState(false);
 
-    // const [visibleBookings, setVisibleBookings] = useState([
-    //     new NextBooking(1, "Shower Service", "Starts in 24hr"),
-    //     new NextBooking(2, "Laundry Cycle", "1 day ago"),
-    //     new NextBooking(3, "Resource Store", "Starts in 24hr"),
-    //     new NextBooking(4, "Job Application Meeting", "Starts in 24hr"),
-    //     new NextBooking(5, "Laundry Cycle", "Starts in 24hr")
-    // ]);
-
-    // const handleDeleteNextBooking = (id) => {
-    //     setVisibleBookings(prevBookings => 
-    //         prevBookings.filter(booking => booking.id !== id)
-    //     );
-    // };
-    
     return (
-        <div className="rounded-xl border-2 border-bright-purple w-80 h-fit p-2 justify-items-center">
+        <div 
+            className={`${isHovering ? 'border-4' : 'border-2'} rounded-xl border-bright-purple w-80 h-fit p-2 justify-items-center cursor-pointer`}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+        >
             <img className="w-80 h-80 rounded-xl" src={`${image}`}/>
-            <p className="font-bold text-lg">
-                { name } 
+            <p className={`${isHovering ? 'underline' : ''} font-bold text-lg`}>
+                { isHovering ? "Details & Guidelines" : name } 
             </p>
         </div>
     );
