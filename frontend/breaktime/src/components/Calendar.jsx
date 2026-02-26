89025d3db80af09ead2d429f20cd413aaa17945a
rockyboo3 <caitlyn.parrishlewis@gmail.com>
2026-02-25 20:25:00 -0500

    F6A- Completely vibe-coded side calendar; dots need testing

--- frontend/breaktime/src/components/Calendar.jsx
+++ frontend/breaktime/src/components/Calendar.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import CalendarPlaceholder from "../assets/navigation/Calendar.png";
+import SideCalendar from "./SideCalendar";
 
-function Calendar() {
+function Calendar({ bookings = [], onViewAllClick }) {
     return (
         <div className="h-full flex flex-col">
-            <div className="flex items-center justify-between mb-4">
-                <h3 className="font-semibold text-xl text-white">Bookings</h3>
-                <span className="text-light-grey opacity-[60%] underline text-sm cursor-pointer">View All Bookings (20+)</span>
-            </div>
-            <img className="h-[170px]" src={CalendarPlaceholder}/>
+            <SideCalendar bookings={bookings} onViewAllClick={onViewAllClick} />
         </div>
     );
 }
